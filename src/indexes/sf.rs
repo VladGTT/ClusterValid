4a0f47e8a7e80380cfdc47a6098f7f949116556b
vlad <vladbozhko003@gmail.com>
2025-04-06 20:02:59 +0300

    Some changes in SF index

--- src/indexes/sf.rs
+++ src/indexes/sf.rs
@@ -54,7 +54,7 @@ impl Index {
         }
         let sum2 = dists.iter().sum::<f64>();
         let temp: f64 = sum1/(n*q)-sum2;
-        let val = 1.-1./temp.exp().exp();
+        let val = 1.-1./temp.exp();
         Ok(val)
 
     }
